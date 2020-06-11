15b26dd49a79520f62ca46b7c37df759f4b2ed8f
jz0001 <jinming.zhang.92@gmail.com>
2020-06-10 23:46:13 -0400

    linking stylesheet

--- rn-starter/src/screens/ComponentsScreen.js
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -2,5 +2,11 @@ import React from 'react';
 import {Text, StyleSheet} from 'react-native';
 
 const ComponentsScreen = () => {
-    return <Text>components screen</Text>
+    return <Text style={styles.textStyle}>components screen</Text>
 }
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 30
+    }
+})
