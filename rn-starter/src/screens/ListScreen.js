0b492f65b2726c48ce0e1e64536cb6987a4abd21
jz0001 <jinming.zhang.92@gmail.com>
2020-06-16 23:21:41 -0400

    imp flatlist

--- rn-starter/src/screens/ListScreen.js
+++ rn-starter/src/screens/ListScreen.js
@@ -1,8 +1,22 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, FlatList} from 'react-native'
 
 const ListScreen = () => {
-    return <Text>List Screen</Text>
+    let friends = [
+        {name: "Friend 1"},
+        {name: "Friend 2"},
+        {name: "Friend 3"},
+        {name: "Friend 4"},
+        {name: "Friend 5"},
+    ]
+    return (
+        <FlatList 
+            data={friends}
+            renderItem={({item})=>{
+                return <Text>{item.name}</Text>
+            }}
+        />
+    )
 }
 
 const styles = StyleSheet.create({})
