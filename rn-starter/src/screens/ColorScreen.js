a9646fcc6e57d51e8661b0cec0fe9aad2072af69
jz0001 <jinming.zhang.92@gmail.com>
2020-06-24 23:31:19 -0400

    adding colors as state

--- rn-starter/src/screens/ColorScreen.js
+++ rn-starter/src/screens/ColorScreen.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { View, StyleSheet, Button } from 'react-native'
 
 const ColorScreen = () => {
+    const [colors, addColors] = useState([])
     return(
         <View>
             <Button title="add color"/>
+            {colors.forEach((color)=>{
+                return <View backgroundColor=></View>
+            })}
         </View>
     )
 }
