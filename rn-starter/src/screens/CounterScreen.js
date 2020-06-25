a0a018443b8f92e1a95c2bb9a896d80b18251cb1
jz0001 <jinming.zhang.92@gmail.com>
2020-06-24 23:07:43 -0400

    adding state using hook

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 
 const CounterScreen = () => {
+    const [counter, setCounter] = useState(0)
+
     return <View>
+        <Button 
+            title="+"
+            onPress={() => setCounter(counter+1)} />
+        <Button 
+            title="-"
+            onPress={() => setCounter(counter-1)} />
         <Text>Counter</Text>
     </View>
 }
